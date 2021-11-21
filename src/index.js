fa988518bb2d25cbfbf18dda6b8efba388e3f8d5
Zayox <amirmoumen06@yahoo.fr>
2021-11-21 14:52:26 +0100

    Dynamic chart added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
+import './css/main.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './js/main';
+import './js/chart';
+import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />,document.getElementById('root'));
+
