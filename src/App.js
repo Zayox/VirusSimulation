d6e41e80441c883549d6326eddd297c94ca9f75b
Zayox <amirmoumen06@yahoo.fr>
2021-11-22 06:04:23 +0100

    Matter js added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './css/App.css';
 import ChartC from "./components/Chart";
+import './css/App.css';
+import Mater from "./components/Matter";
 
 
 function App() {
   return (
     <div className="App">
         <ChartC />
+        <Mater className="mater" />
     </div>
+
   );
 }
 
