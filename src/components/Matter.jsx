0f8618337173ffd50f62c3073c986c57afd009d6
Zayox <amirmoumen06@yahoo.fr>
2021-12-03 00:26:40 +0100

    pythagorean theorem collision started

--- src/components/Matter.jsx
+++ src/components/Matter.jsx
@@ -17,16 +17,43 @@ const Mater = () => {
 // create a renderer
     var render = Render.create({
         element: document.body,
-        engine: engine
+        engine: engine,
+        wireframes: false,
+        background: 'rgb(255,0,0)' // or '#ff0000' or other valid color string
+
     });
 
+    var boxA = Bodies.circle(200, 200, 20, 20,);
+
+    function randomMovement(){
+
+        setInterval(function(){
+            let randomNumber = Math.random()*760;
+            let randomNumber2 = Math.random()*560;
+
+            boxA = Bodies.circle(randomNumber, randomNumber2, 20, {
+                render: {
+                    fillStyle: 'red'
+                }
+            });
+            Composite.add(engine.world, [boxA]);
+            },1500)
+
+
+    }
+
+    randomMovement();
+
+
 // create two boxes and a ground
-    var boxA = Bodies.rectangle(400, 200, 80, 80);
-    var boxB = Bodies.rectangle(450, 50, 80, 80);
-    var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+    var ground = Bodies.rectangle(400, 610, 810, 50, { isStatic: true });
+    var leftWall = Bodies.rectangle(-45, 210, 110, 800, { isStatic: true });
+    var rightWall = Bodies.rectangle(840, 210, 110, 800, { isStatic: true });
+    var ceiling = Bodies.rectangle(400, -10, 810, 50, { isStatic: true });
+    engine.world.gravity.y = 0;
 
 // add all of the bodies to the world
-    Composite.add(engine.world, [boxA, boxB, ground]);
+    Composite.add(engine.world, [ground,leftWall,rightWall,ceiling]);
 
 // run the renderer
     Render.run(render);
