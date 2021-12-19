23836f066b15d9ed4d169eb20d78eaeae2054f5b
amirmoumen <amirmoumen06@yahoo.fr>
2021-12-19 12:25:07 -0800

    laptop responsive

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import './css/main.css';
+import './css/mainLaptop.css';
 import './css/App.css';
 import App from './App';
 import './js/main';
